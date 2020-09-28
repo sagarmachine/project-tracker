2fc17bdbbfecba2d6f318f1641bb35003931d978
sagarmachine <sagarmonkey800@gmail.com>
2020-09-28 13:43:35 +0530

    project service abstracted

--- src/main/java/com/highbrowape/demo/service/IProjectService.java
+++ src/main/java/com/highbrowape/demo/service/IProjectService.java
@@ -1,20 +1,55 @@
 package com.highbrowape.demo.service;
 
 
-import com.highbrowape.demo.dto.input.ProjectAdd;
-import com.highbrowape.demo.dto.input.ProjectUpdate;
-import com.highbrowape.demo.dto.output.ProjectDashboardDto;
-import com.highbrowape.demo.dto.output.ProjectListDto;
+import com.highbrowape.demo.dto.input.*;
+
+import com.highbrowape.demo.entity.Authority;
 import com.highbrowape.demo.entity.Project;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public interface IProjectService {
 
-    ResponseEntity<?>  addProject(ProjectAdd projectAdd, String loggedInEmail);
-    ResponseEntity<?>  updateProject(ProjectUpdate projectUpdate, String loggedInEmail);
-//    ResponseEntity<?>  getProject(Long projectId, String loggedInEmail);
-    ResponseEntity<?>  getProjectDashboard(Long projectId, String loggedInEmail);
+ public interface IProjectService {
+
+    ResponseEntity<?> addProject(ProjectAdd projectAdd, String loggedInEmail);
+
+    ResponseEntity<?> updateProject(ProjectUpdate projectUpdate, String loggedInEmail);
+
+    ResponseEntity<?> getProjectDashboard(Long projectId, String loggedInEmail);
+
     ResponseEntity<?> getProjectList(int pageNumber, String loggedInEmail);
+
     ResponseEntity<Project> getProjectDetail(Long projectId, String loggedInEmail);
+
     ResponseEntity<?> getMyCreatedProjectList(int pageNumber, String loggedInEmail);
+
+
+
+
+    //project DB id
+     ResponseEntity<?>  addMemberToProject(Long id, ProjectMemberDto projectMemberDto, String loggedInEmail);
+    //member DB id
+     ResponseEntity<?>  updateMemberAuthorityToProject(Long id, @PathVariable("authority") Authority authority, String loggedInEmail);
+     //member DB id
+     ResponseEntity<?>  removeMemberFromProject(Long id, String loggedInEmail);
+
+
+
+
+
+     ResponseEntity<?>  addNoteToProject(Long id, NoteDto noteDto, String loggedInEmail);
+     ResponseEntity<?>  updateNoteOfProject(Long id, NoteDto noteDto,String loggedInEmail);
+     ResponseEntity<?>  removeNoteFromProject(Long id, String loggedInEmail);
+
+
+
+
+
+
+     ResponseEntity<?> addLinkToProject( Long id, LinkDto linkDto, String loggedInEmail);
+
+     ResponseEntity<?> updateLinkOfProject( Long id, LinkDto linkDto, String loggedInEmail);
+
+     ResponseEntity<?> removeLinkFromProject( Long id, String loggedInEmail);
+
 }
