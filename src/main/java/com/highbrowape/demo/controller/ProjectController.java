e2e7cb167155de3fbc5845d91b7d5c7ca45843f1
sagarmachine <sagarmonkey800@gmail.com>
2020-09-28 11:55:10 +0530

    note link member seperate controllers added

--- src/main/java/com/highbrowape/demo/controller/ProjectController.java
+++ src/main/java/com/highbrowape/demo/controller/ProjectController.java
@@ -1,17 +1,14 @@
 package com.highbrowape.demo.controller;
 
 
-import com.highbrowape.demo.dto.input.ProjectAdd;
-import com.highbrowape.demo.dto.input.ProjectUpdate;
-import com.highbrowape.demo.dto.output.ProjectDashboardDto;
-import com.highbrowape.demo.dto.output.ProjectListDto;
+import com.highbrowape.demo.dto.input.*;
+import com.highbrowape.demo.entity.Authority;
 import com.highbrowape.demo.entity.Project;
 import com.highbrowape.demo.repository.ProjectRepository;
 import com.highbrowape.demo.service.IProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.security.Principal;
 
 @RestController
@@ -62,5 +59,54 @@ public class ProjectController {
     public  ResponseEntity<Project> getProjectDetailView(@PathVariable("id")Long id, Principal principal){
         return projectService.getProjectDetail(id,principal.getName());
     }
+
+
+    @PostMapping("/{id}/member")
+    public ResponseEntity<?>  addMemberToProject(@PathVariable("id")Long id, @RequestBody ProjectMemberDto projectMemberDto, Principal principal){
+        return null;
+    }
+    @PostMapping("/member/{id}/authority/{authority}")
+    public ResponseEntity<?>  updateMemberAuthorityToProject(@PathVariable("id")Long id, @PathVariable("authority") Authority authority, Principal principal){
+        return null;
+    }
+    @DeleteMapping("/member/{id}")
+    public ResponseEntity<?>  removeMemberFromProject(@PathVariable("id")Long id, Principal principal){
+
+        return null;
+    }
+
+
+
+    @PostMapping("/{id}/note")
+    public ResponseEntity<?>  addNoteToProject(@PathVariable("id")Long id, @RequestBody NoteDto noteDto,Principal principal){
+        return null;
+    }
+    @PostMapping("/note/{id}")
+    public ResponseEntity<?>  updateNoteOfProject(@PathVariable("id")Long id, @RequestBody NoteDto noteDto,Principal principal){
+        return null;
+    }
+    @DeleteMapping("/note/{id}")
+    public ResponseEntity<?>  removeNoteFromProject(@PathVariable("id")Long id, Principal principal){
+
+        return null;
+    }
+
+
+
+
+    @PostMapping("/{id}/link")
+    public ResponseEntity<?>  addLinkToProject(@PathVariable("id")Long id, @RequestBody LinkDto linkDto, Principal principal){
+        return null;
+    }
+    @PostMapping("/link/{id}")
+    public ResponseEntity<?>  updateLinkOfProject(@PathVariable("id")Long id, @RequestBody LinkDto linkDto,Principal principal){
+        return null;
+    }
+    @DeleteMapping("/link/{id}")
+    public ResponseEntity<?>  removeLinkFromProject(@PathVariable("id")Long id, Principal principal){
+
+        return null;
+    }
+
 
 }
--- src/main/java/com/highbrowape/demo/entity/Mission.java
+++ src/main/java/com/highbrowape/demo/entity/Mission.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
