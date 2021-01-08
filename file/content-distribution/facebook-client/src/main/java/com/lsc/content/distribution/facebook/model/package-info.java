86186bd929e54bd175f63acd03d1d498e9fddf33
sagarmachine <sagarmonkey800@gmail.com>
2021-01-08 11:49:46 +0530

    merged

--- file/content-distribution/facebook-client/src/main/java/com/lsc/content/distribution/facebook/model/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = XMLConstants.PPC_DISTRIBUTION_SERVICE_FACEBOOK_NS_URI, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.lsc.content.distribution.facebook.model;
-
-import com.lsc.content.distribution.facebook.XMLConstants;
- 
